db519e171ea578ba3f85e311399e35da3e1eced1
Cryptomiester <77497858+crypt0miester@users.noreply.github.com>
2023-08-31 13:02:36 +0000

    fix mintNewNft()

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,29 @@
 import { Connection, Keypair } from "@solana/web3.js";
-import { TOKEN_DESCRIPTION, DEVNET, MAINNET, TOKEN_METADATA_IMAGE, TOKEN_SYMBOL, TOKEN_NAME } from "./constants";
+import {
+    TOKEN_DESCRIPTION,
+    DEVNET,
+    MAINNET,
+    TOKEN_METADATA_IMAGE,
+    TOKEN_SYMBOL,
+    TOKEN_NAME,
+} from "./constants";
 import { UploadMetadataInput } from "@metaplex-foundation/js";
-import rawKeypair from '../wallet/keypair.json';
+import rawKeypair from "../wallet/keypair.json";
 
 // connection
-export const CONNECTION = new Connection(DEVNET, 'confirmed');
+export const CONNECTION = new Connection(DEVNET, "confirmed");
 
 // SEMI-FUNGIBLE MINT CONFIG
 export const MINT_CONFIG = {
-  numDecimals: 0,
-  numberTokens: 1,
+    numDecimals: 0,
+    numberTokens: 1,
 };
 
 export const TOKEN_METADATA: UploadMetadataInput = {
-  name: TOKEN_NAME,
-  symbol: TOKEN_SYMBOL,
-  description: TOKEN_DESCRIPTION,
-  image: TOKEN_METADATA_IMAGE,
+    name: TOKEN_NAME,
+    symbol: TOKEN_SYMBOL,
+    description: TOKEN_DESCRIPTION,
+    image: TOKEN_METADATA_IMAGE,
 };
 
 export const MINT_AUTHORITY = Keypair.fromSecretKey(new Uint8Array(rawKeypair));
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,36 @@
-import * as config from './config';
+import * as config from "./config";
 import * as fs from "fs";
 import { setTimeout } from "timers/promises";
-import { dedupWallets, mintNewSFT, mintTokensToOnePubkey, nftMultiMinter, readCSVFile, tokenMultiMinter, walletAmountType } from './utils';
-import { PublicKey } from '@solana/web3.js';
+import {
+    dedupWallets,
+    mintNewNFT,
+    mintTokensToOnePubkey,
+    nftMultiMinter,
+    readCSVFile,
+    tokenMultiMinter,
+    walletAmountType,
+} from "./utils";
+import { PublicKey } from "@solana/web3.js";
 
-import wallets from '../data/wallets.json';
+import wallets from "../data/wallets.json";
 
 // uncomment the code step by step.
 async function main() {
-  /// 0.0 yarn install
-  /// 0.a CHECK all the data in .env and constants.ts
-  /// 0.b create a new wallet and save it wallet/keypair.json.
-  /// 0.c make sure the images/background.jpg is correct - it is going to be the nft background
+    /// 0.0 yarn install
+    /// 0.a CHECK all the data in .env and constants.ts
+    /// 0.b create a new wallet and save it wallet/keypair.json.
+    /// 0.c make sure the images/background.jpg is correct - it is going to be the nft background
 
-  /// 1.a TEST airdrop to one pubkey (string)
-  //await mintTokensToOnePubkey(await mintNewSFT(), "Gm4FvmDqQxNCqRdmynNknqdYcMwitLMfqY8aLFaxDwSq")
+    /// 1.a TEST airdrop to one pubkey (string)
+    //await mintTokensToOnePubkey(await mintNewNFT(), "Gm4FvmDqQxNCqRdmynNknqdYcMwitLMfqY8aLFaxDwSq")
 
-  /// 1. MINT NFTs
-  console.log(wallets);
+    /// 1. MINT NFTs
+    console.log(wallets);
 
-  await mintNewSFT(new PublicKey("Gm4FvmDqQxNCqRdmynNknqdYcMwitLMfqY8aLFaxDwSq"))
+    // await mintNewNFT(new PublicKey("Gm4FvmDqQxNCqRdmynNknqdYcMwitLMfqY8aLFaxDwSq"))
 
-  //console.log(config.MINT_AUTHORITY.publicKey);
-  //await nftMultiMinter(wallets);
+    //console.log(config.MINT_AUTHORITY.publicKey);
+    await nftMultiMinter(wallets);
 }
 
-main()
+main();
